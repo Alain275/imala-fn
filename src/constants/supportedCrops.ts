e79b1402bd5f3e4118212d517c6af43e4c574b24
Alain275 <alainshema275@gmail.com>
2026-08-26 21:52:16 +0200

    new

--- src/constants/supportedCrops.ts
+++ src/constants/supportedCrops.ts
@@ -1,6 +1,6 @@
 export const SUPPORTED_CROPS = ['Irish potatoes', 'Maize', 'Beans'] as const
 // The portal exposes only crops with an active multiview ML model. Add a crop
 // here only after its dedicated model has been trained and enabled by the API.
-export const DISEASE_DETECTION_CROPS = ['Maize'] as const
+export const DISEASE_DETECTION_CROPS = ['Irish potatoes', 'Maize'] as const
 
 export type SupportedCrop = (typeof SUPPORTED_CROPS)[number]
